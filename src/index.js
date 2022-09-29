4d357af15bcf844c0832bda72a31858ab12a2bd9
Olesya Asanova <asa88.88@bk.ru>
2022-09-29 12:14:39 +0300

    завершение проекта

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import slider from './modules/slider'
 import calc from './modules/calc'
 import sendForm from './modules/sendForm'
 
-timer('21 september 2022')
+timer('21 september 2023')
 menu()
 modal()
 validate()
